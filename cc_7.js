2d53f9f5f5e782ac73fec86d0099f4443815324f
Jbarb25 <barboza205@usf.edu>
2025-02-10 18:32:10 -0500

    Task 2 - Employee Hourly Wage Calculation
    Declare the function to calculate the hourly wage with the parameters of salary and hours per week. Calculate the hourly wage using the parameters plugged into the formula. Return the resulting hourly wage value to where it is called. Call the function calculateHourlyWage, plug in the values into the parameters, and log the wage to the console.

--- cc_7.js
+++ cc_7.js
@@ -8,3 +8,11 @@ console.log(`Final Invoice Amount: $${calculateInvoice(100, 0.08,5).toFixed(2)}`
 console.log(`Final Invoice Amount: $${calculateInvoice(500, 0.1, 20).toFixed(2)}`)
 
 
+//Task 2: Function Expression
+function calculateHourlyWage(salary, hoursPerWeek){  //Declare the function to calculate the hourly wage with the parameters of salary and hours per week
+    hourlyWage = salary / (hoursPerWeek * 52)  //Calculate the hourly wage using the parameters pluged into the formula
+    return hourlyWage}  //Return the resulting hourly wage value to where it is called
+
+//Call the function calculateHourlyWage, plug in the values into the parameters, and log the wage to the console
+console.log(`Hourly Wage: $${calculateHourlyWage(52000,40).toFixed(2)}`)
+console.log(`Hourly Wage: $${calculateHourlyWage(75000,35).toFixed(2)}`)
