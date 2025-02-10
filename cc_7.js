e8a43b27c6e23dde9ea2f7e55cb7566eb30c474a
Jbarb25 <barboza205@usf.edu>
2025-02-10 18:46:06 -0500

    Task 3 - Customer Loyalty Discount
    The arrow function is created to sort the years into ranges and will apply the calculation to find the discounted price based on the values plugged into the parameters when called.

--- cc_7.js
+++ cc_7.js
@@ -10,9 +10,23 @@ console.log(`Final Invoice Amount: $${calculateInvoice(500, 0.1, 20).toFixed(2)}
 
 //Task 2: Function Expression
 function calculateHourlyWage(salary, hoursPerWeek){  //Declare the function to calculate the hourly wage with the parameters of salary and hours per week
-    hourlyWage = salary / (hoursPerWeek * 52)  //Calculate the hourly wage using the parameters pluged into the formula
+    hourlyWage = salary / (hoursPerWeek * 52)  //Calculate the hourly wage using the parameters plugged into the formula
     return hourlyWage}  //Return the resulting hourly wage value to where it is called
 
 //Call the function calculateHourlyWage, plug in the values into the parameters, and log the wage to the console
 console.log(`Hourly Wage: $${calculateHourlyWage(52000,40).toFixed(2)}`)
 console.log(`Hourly Wage: $${calculateHourlyWage(75000,35).toFixed(2)}`)
+
+
+//Task 3: Arrow Function
+const calculateLoyaltyDiscount = (amount, years) => {  //Write an arrow function that calculates loyalty discount with the parameters of amount and years
+    let discountedPrice = 0;   //The discountedPrice will start at 0 to assign the calculated value to to return it when called
+    if (years >= 5) discountedPrice = (amount -(amount * 0.15)) ;
+    else if (years >= 3) discountedPrice = (amount - (amount * 0.10));
+    else if (years < 3) discountedPrice = (amount - (amount * 0.05));
+    else discountedPrice = amount;  //The function will run through how many years, and based on what range it falls into, it will run the discount formula to find the discounted Price
+    return discountedPrice}
+
+//The arrow function will return the discounted price value to the console based on the parameters it plugs in
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(100, 6).toFixed(2)}`)
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(200, 2).toFixed(2)}`)
